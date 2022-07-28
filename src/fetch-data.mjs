69d41b4e49b938de2a23fab88a2e20634bc665ed
Jack-Zhang-1314 <zyj17715640603@gmail.com>
2022-07-29 06:30:19 +0800

    update fetch data

--- src/fetch-data.mjs
+++ src/fetch-data.mjs
@@ -1,11 +1,14 @@
-import fetch from 'node-fetch'
 import * as fs from "node:fs"
+import { Octokit } from '@octokit/core'
 
-// fetch(
-//   "https://gist.githubusercontent.com/Jack-Zhang-1314/da0f0b1216210c9a949bf55ff843299f/raw/e5757ad9b2cecc5d6089628ceae244d73f0e848b/lemoji.json",
-//   )
-//   .then((res) => res.body.pipe(fs.createWriteStream('lemoji.json')))
+const octokit = new Octokit({
+  auth: process.env["GITHUB_TOKEN"],
+})
 
-fetch("https://gist.github.com/Jack-Zhang-1314/da0f0b1216210c9a949bf55ff843299f").then(
-  (res) => res.text()
-).then(res => console.log(res))
+const { data: { files } } = await octokit.request('GET /gists/{gist_id}', {
+  gist_id: 'da0f0b1216210c9a949bf55ff843299f'
+})
+
+fs.writeFile("./lemoji.json", files["lemoji.json"].content, (err) => {
+  if (err) console.log(err)
+})
